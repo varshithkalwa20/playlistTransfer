70ae8acfb2bc181490e2a3aed8aabb5794371dd9
varshithkalwa20 <87393192+varshithkalwa20@users.noreply.github.com>
2024-12-22 22:42:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,7 @@ def transfer_playlist():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Use host 0.0.0.0 for external access
+
+    
